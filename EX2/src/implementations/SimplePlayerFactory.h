f628e6c6ecb1dd8aacb3098d088536d7a01971d1
yovel <yovelhatan@gmail.com>
2025-06-04 09:50:18 +0300

    put simpalePlayerFactory as a comment

--- EX2/src/implementations/SimplePlayerFactory.h
+++ EX2/src/implementations/SimplePlayerFactory.h
@@ -7,15 +7,15 @@
 
 class SimplePlayerFactory : public PlayerFactory {
     public:
-        explicit SimplePlayerFactory(const Config& config);
+        //explicit SimplePlayerFactory(const Config& config);
         virtual unique_ptr<Player> create(int player_index, size_t x, size_t y, size_t max_steps, size_t num_shells ) const override;
 
-    private:
-        static constexpr int DEFAULT_RADIUS = 3;
-        static constexpr const char* DEFAULT_PLAYER_TYPE = "simple";
+    // private:
+    //     static constexpr int DEFAULT_RADIUS = 3;
+    //     static constexpr const char* DEFAULT_PLAYER_TYPE = "simple";
 
-        std::string player_type;
-        int radius_value;
+    //     std::string player_type;
+    //     int radius_value;
 };
 
 #endif
